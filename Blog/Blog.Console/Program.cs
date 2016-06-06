363f23e9307698755700429522e0dc5d5eab191b
nikolay <marakasovniko@yandex.ru>
2016-06-06 22:02:28 +0300

    add new wrapper for null objects

---
+++ Blog/Blog.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Blog.Infrastructure;
+using Blog.Model.Post;
+
+namespace Blog.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Maybe<Post> post = new Post(new PostContent("Hello", "c# this is supper", "c# with me supper"),
+                new PublishingDates(DateTime.Today, DateTime.Now));
+
+            Maybe<Post> post1 = Post.Create(new PostContent("Hello", "c# this is supper", "c# with me supper"),
+                new PublishingDates(DateTime.Today, DateTime.Now));
+
+            System.Console.WriteLine(post);
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ Blog/Domain.Model/Post/Post.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace Blog.Model.Post
+{
+    public class Post
+    {
+        public Post(PostContent content, PublishingDates publishingDates)
+        {
+            Contract.Requires(content != null && publishingDates != null);
+            Content = content;
+            PublishingDates = publishingDates;
+        }
+        public PostContent Content { get; set; }
+        public PublishingDates PublishingDates { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Content.Title}, {Content.Summary}, {PublishingDates.PublishingDateBeg}";
+        }
+
+        public static Post Create(PostContent content, PublishingDates publishingDates)
+        {
+            return null;
+            //return new Post(content, publishingDates);
+        }
+    }
+
+    public class PublishingDates
+    {
+        public PublishingDates(DateTime dateBeg, DateTime dateEnd)
+        {
+            Contract.Requires(dateBeg < dateEnd);
+            PublishingDateBeg = dateBeg;
+            PublishingDateEnd = dateEnd;
+        }
+        public DateTime PublishingDateBeg { get; private set; }
+        public DateTime PublishingDateEnd { get; private set; }
+    }
+
+    public class PostContent
+    {
+        public PostContent(string title, string summary, string text)
+        {
+            Contract.Requires(!string.IsNullOrWhiteSpace(title) && !string.IsNullOrWhiteSpace(summary) &&
+                              !string.IsNullOrWhiteSpace(text));
+            Title = title;
+            Summary = summary;
+            Text = text;
+        }
+        public string Title { get; private set; }
+        public string Summary { get; private set; }
+        public string Text { get; private set; }
+    }
+}
